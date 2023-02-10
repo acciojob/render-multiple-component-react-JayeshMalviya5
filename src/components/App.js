e9abb2e883d6dc6618c860c42a45a8fe481ca8e0
jayeshmalviya5 <jayeshmalviya10@gmail.com>
2023-02-10 22:53:38 +0530

    jk

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">      
+     
+    </div>
+  );
+};
 
 export default App;
